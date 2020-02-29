16105ec2a91139af7be7ba4df9cc3b101ef56f63
Kaycheng <kaycheng717@gmail.com>
2020-02-29 23:38:19 +0800

    Create Story model and controller

---
+++ app/models/story.rb
@@ -0,0 +1,4 @@
+class Story < ApplicationRecord
+  belongs_to :user, dependent: :destroy
+  validates :title, presence: true
+end
---
+++ db/migrate/20200229152838_create_stories.rb
@@ -0,0 +1,12 @@
+class CreateStories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :stories do |t|
+      t.string :title
+      t.text :content
+      t.string :status, default: 'draft'
+      t.references :user
+
+      t.timestamps
+    end
+  end
+end
