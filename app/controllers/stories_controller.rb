b978c2585a48077ceaf4afbac9c24b875ec7aba0
Kaycheng <kaycheng717@gmail.com>
2020-03-01 13:43:52 +0800

    Finish stories controller actions

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,39 @@
 class StoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_story, only: [:edit, :show, :update]
+  
+  def index
+    @stories = current_user.stories.order(created_at: :desc)
+  end
+
+  def new
+    @story = current_user.stories.new
+  end
+
+  def create
+    @story = current_user.stories.new(story_params)
+    if @story.save
+      redirect_to stories_path, notice: "Way to go!"
+    else
+      render 'new', notice: "Something is in error."
+    end
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to story_path, notice: "Updated successfully"
+    else
+      render 'edit', notice: "Somthing is in error."
+    end
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:title, :content, :status)
+  end
+
+  def find_story
+    @story = current_user.stories.find(params[:id])
+  end
 end
