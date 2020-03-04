1724322f4f30b67b7da3961e0cefce1e3bf0a266
Kaycheng <kaycheng717@gmail.com>
2020-03-04 14:26:50 +0800

    Setting soft deleted of stories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_152838) do
+ActiveRecord::Schema.define(version: 2020_03_04_061305) do
 
   create_table "stories", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_02_29_152838) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_stories_on_deleted_at"
     t.index ["user_id"], name: "index_stories_on_user_id"
   end
 
