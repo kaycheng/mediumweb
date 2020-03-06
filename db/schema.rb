5f7d89d81869081daedafe2ebf5482ee3ff7c52a
Kaycheng <kaycheng717@gmail.com>
2020-03-07 00:03:17 +0800

    Create devise:controller users to add username and intro

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_030845) do
+ActiveRecord::Schema.define(version: 2020_03_06_123353) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -45,8 +45,11 @@ ActiveRecord::Schema.define(version: 2020_03_06_030845) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.text "intro"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
