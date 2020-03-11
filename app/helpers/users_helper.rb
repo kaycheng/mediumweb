086b35fe14e7801dd156e8039d41961568d01514
Kaycheng <kaycheng717@gmail.com>
2020-03-11 15:55:25 +0800

    Change the user-avatar shape from square to circle

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper  
   def avatar(user, size: 250)
-    image_tag user.avatar.variant(resize: "#{size}x#{size}") if user.avatar.attached?
+    image_tag user.avatar.variant(resize: "#{size}x#{size}"), class: "user-avatar" if user.avatar.attached?
   end
 end
