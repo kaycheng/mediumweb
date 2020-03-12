0caa3f20134643a2a9ce1a8b7b2be6bb4f771a38
Kaycheng <kaycheng717@gmail.com>
2020-03-12 20:44:08 +0800

    Add content of welcome#show page and change cover-image style

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  before_action :find_story, only: [:show]
   def index
     # @stories = Story.order(created_at: :desc).includes(:user)
     # @stories = Story.where(status: 'published').order(created_at: :desc).includes(:user)
@@ -8,7 +9,13 @@ class WelcomeController < ApplicationController
   
   def show
   end
-
+  
   def user
+  end
+  
+  private
+  
+  def find_story
+    @story = Story.friendly.find(params[:story_id])
   end
 end
