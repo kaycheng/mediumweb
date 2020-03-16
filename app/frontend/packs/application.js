0a46aa77cf153cf4901af318bef7c5de08759686
Kaycheng <kaycheng717@gmail.com>
2020-03-16 14:51:56 +0800

    Add fontawesome in project

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -10,6 +10,8 @@ require("channels")
 
 import '../stylesheets/application'
 import './bootstrap_custom.js'
+import '@fortawesome/fontawesome-free'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
